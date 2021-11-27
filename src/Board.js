fe819408c6bcfe697fb88f3315ee569e42090a7f
Mo <rahim.muhammad@pm.me>
2021-11-27 10:59:21 +0100

    rewrite renderSquare in for loops

--- src/Board.js
+++ src/Board.js
@@ -10,25 +10,40 @@ export class Board extends React.Component {
     );
   }
 
+  renderRows(r) {
+    let res = [];
+    for (var i = 0; i < 3; i ++ ) {
+      res.push(this.renderSquare(3*r+i));
+    }
+    return <div>{res}</div>
+  }
+
   render() {
+    let rows = [];
+    for (var i = 0; i < 3; i++) {
+      rows.push(this.renderRows(i));
+    }
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+      <div>{rows}</div>
+    )
+    // return (
+    //   <div>
+    //     <div className="board-row">
+    //       {this.renderSquare(0)}
+    //       {this.renderSquare(1)}
+    //       {this.renderSquare(2)}
+    //     </div>
+    //     <div className="board-row">
+    //       {this.renderSquare(3)}
+    //       {this.renderSquare(4)}
+    //       {this.renderSquare(5)}
+    //     </div>
+    //     <div className="board-row">
+    //       {this.renderSquare(6)}
+    //       {this.renderSquare(7)}
+    //       {this.renderSquare(8)}
+    //     </div>
+    //   </div>
+    // );
   }
 }
